11ce2046ec969ff347f2b7c850d80aab12977cf5
MalKhar <brunocbranco16@gmail.com>
2024-04-17 17:49:14 -0300

    added filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Ng2SearchPipe } from 'ng2-search-filter';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { generate } from 'rxjs';
+import posts from './postsExport';
 
 @Component({
   selector: 'app-root',
@@ -14,64 +15,95 @@ import { generate } from 'rxjs';
 export class AppComponent {
   title = 'consystreetknowledge';
 }
-
-
-
-
-
-
-const example1 = ` <div class="contentImage">
-  <div class="image"><img src="https://placehold.co/300x200" alt=""></div>
-  <h2>Lorem ipsum</h2>
-  <p class="contentPreview">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint minus magnam necessitatibus possimus sequi ipsa pariatur quisquam dolor eos corporis vero, tempore ratione. Eaque deserunt repellat laboriosam eum velit?</p>
-  </div>`
-
-  const example2 = ` <div class="contentImage">
-  <div class="image"><img src="https://placehold.co/300x200" alt=""></div>
-  <h2>Lorem ipsum</h2>
-  <p class="contentPreview">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint minus magnam necessitatibus possimus sequi ipsa pariatur quisquam dolor eos corporis vero, tempore ratione. Eaque deserunt repellat laboriosam eum velit?</p>
-  </div>`
-
-  const example3 = ` <div class="contentImage">
-  <div class="image"><img src="https://placehold.co/300x200" alt=""></div>
-  <h2>Lorem ipsum</h2>
-  <p class="contentPreview">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint minus magnam necessitatibus possimus sequi ipsa pariatur quisquam dolor eos corporis vero, tempore ratione. Eaque deserunt repellat laboriosam eum velit?</p>
-  </div>`
-
-  const example4 = ` <div class="contentImage">
-  <div class="image"><img src="https://placehold.co/300x200" alt=""></div>
-  <h2>Lorem ipsum</h2>
-  <p class="contentPreview">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint minus magnam necessitatibus possimus sequi ipsa pariatur quisquam dolor eos corporis vero, tempore ratione. Eaque deserunt repellat laboriosam eum velit?</p>
-  </div>`
-
-  const example5 = ` <div class="contentImage">
-  <div class="image"><img src="https://placehold.co/300x200" alt=""></div>
-  <h2>Lorem ipsum</h2>
-  <p class="contentPreview">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint minus magnam necessitatibus possimus sequi ipsa pariatur quisquam dolor eos corporis vero, tempore ratione. Eaque deserunt repellat laboriosam eum velit?</p>
-  </div>`
   
-const posts = [example1, example2, example3, example4, example5];
 
 //espera DOM
 window.onload = init;
 function init(){
 
 const searchResult = document.getElementById("searchResult");
-console.log(searchResult);
+
+var submitInput = (<GlobalEventHandlers>document.getElementById("input"));
+submitInput.onkeydown = filter;
+submitInput.onkeyup = filter;
 
 function constructor(){
-  posts.forEach(post => {
+  posts.forEach((post, i) => {
     const postContent = document.createElement('div');
     postContent.classList.add('post');
-    console.log(postContent);
-    postContent.innerHTML = post;
+    postContent.setAttribute('id', i.toString())
+
+    const divImg = document.createElement('div');
+    divImg.classList.add('thumbImg');
+    
+    const link = document.createElement('a');
+    link.setAttribute('href', posts[i].link);
+    link.innerHTML = posts[i].title;
+    link.setAttribute('target', "_blank");
+
+    const img = document.createElement('img');
+    img.setAttribute('src', posts[i].imgURL);
+    
+    const linkImg = document.createElement('a');
+    linkImg.setAttribute('href', posts[i].link);
+    linkImg.setAttribute('target', "_blank");
+    linkImg?.appendChild(img);
+    
+    const title = document.createElement('h2');
+    title?.appendChild(link)
+
+    const description = document.createElement('p');
+    description.classList.add('contentPreview');
+    description.innerHTML = posts[i].description;
+    
     searchResult?.appendChild(postContent);
     
-  });
+    postContent?.appendChild(divImg);
+    divImg?.appendChild(linkImg);
+    postContent?.appendChild(title);
+    postContent?.appendChild(description);
+  });  
+}
+constructor();
+function clean(){
+  while(searchResult?.firstChild){
+    searchResult?.removeChild(searchResult.firstChild)
+  }
+}
 
+
+
+function filter(){
+  let inputValue = (<HTMLInputElement>document.getElementById("input")).value;
+
+  clean();
+  constructor();
+  
+  posts.forEach((post, i) => {
+    let currentTrue = document.getElementById(i.toString());
+    if (inputValue == ""){
+      clean();
+      constructor();
+    }
+    else if(!posts[i].tags.includes(inputValue)){
+      currentTrue?.remove();
+    }
+  });
+  
   
 }
 
-constructor();
+
+
+
+
+  
+  
+
+
+
+
+
 
 }
+
---
+++ src/app/postsExport.ts
@@ -0,0 +1,51 @@
+const posts = [
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Att check SGF",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "Como atualizar o Check ou SGF. Necessário acesso ao FTP.",
+        "tags": ["check", "sgf"],
+    },
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Check não abre",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "Teste teste",
+        "tags": ["check"],
+  },
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Certificado SEFAZ",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "A chess bot",
+        "tags": ["check", "sgf", "NFe", "SEFAZ"],
+  },
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Teste",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "A company",
+        "tags": ["teste"],
+  },
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Cliente quer alterar etiqueta",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "Adam Sandler",
+        "tags": ["sgf", "etiqueta", "impressora"],
+  },
+    {
+        "imgURL": "https://placehold.co/300x200",
+        "title": "Como lidar com cliente bravo",
+        "link": "https://www.youtube.com/watch?v=q652jp6YVqk",
+        "description": "Adam Sandler",
+        "tags": ["bravo", "soft skill", "lidar", "como lidar"],
+    }
+
+    
+
+]
+
+
+
+export default posts;
